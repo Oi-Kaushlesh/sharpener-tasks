3661367c6c7152c62ea67ad230dbceabe67ddd15
Oi-Kaushlesh <kaushlesh2611singh@gmail.com>
2024-02-27 01:51:56 +0530

    querySelector & querySelectorAll Methods

--- dom manipulation/index.js
+++ dom manipulation/index.js
@@ -1,6 +1,31 @@
-// Write your code below:
-const list = document.getElementsByTagName('li')
-list[4].style.color = 'blue';
-for(let i = 0 ; i<list.length ; i++){
-  list[i].style.fontStyle = 'italic'
+// Write the code as shown in the video below:
+
+// Write answer to the questions asked below:
+const mainheading = document.querySelector('#main-heading')
+mainheading.style.textAlign = 'right'
+const basketheading = document.querySelector('#basket-heading')
+basketheading.style.paddingLeft = '30px'
+basketheading.style.color ='brown'
+const fruits = document.querySelector('.fruits')
+fruits.style.background = 'grey';
+fruits.style.padding = '30px'
+fruits.style.margin = '30px'
+fruits.style.listStyleType = 'none'
+fruits.style.width = '50%'
+fruits.style.borderRadius = '5px'
+const fruititem = document.querySelectorAll('.fruit')
+for(let i = 0 ; i<fruititem.length ; i++){
+  fruititem[i].style.padding = '10px'
+  fruititem[i].style.margin = '10px'
+  fruititem[i].style.borderRadius = '5px'
+}
+const oddfruit = document.querySelectorAll('.fruit:nth-child(odd)')
+for(let i = 0 ; i<oddfruit.length; i++){
+  oddfruit[i].style.background = 'lightgrey'
+}
+const evenfruit = document.querySelectorAll('.fruit:nth-child(even)')
+for(let i = 0 ; i<oddfruit.length ; i++){
+  evenfruit[i].style.background = 'brown'
+    evenfruit[i].style.color = 'white'
+
 }
