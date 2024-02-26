ac99b8d34840d8e016dba74d5848d31be202e4f5
Oi-Kaushlesh <kaushlesh2611singh@gmail.com>
2024-02-27 00:44:00 +0530

    getelementById

---
+++ getelemntbyID/index.js
@@ -0,0 +1,11 @@
+// Write your code below:
+const header = document.getElementById("header")
+header.style.backgroundColor = "green";
+header.style.borderBottom = "3px solid orange"
+const mainHeading = document.getElementById("main-heading")
+mainHeading.style.color = 'orange';
+
+const basketHeading = document.getElementById("basket-heading")
+basketHeading.style.color = "green"
+const thnaks = document.getElementById('thanks');
+thanks.innerHTML = '<p>Please visit us again</p>'
