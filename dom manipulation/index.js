496078f5d2f3b52b2af145430dc151aa3a881e25
Oi-Kaushlesh <kaushlesh2611singh@gmail.com>
2024-02-29 23:09:32 +0530

    Understandin Local Storage

--- dom manipulation/index.js
+++ dom manipulation/index.js
@@ -1,41 +1,12 @@
-// Add the Edit Button:
+// Write your code below:
+function savetolocalStorage(event){
+   event.preventDefault();
+   const nameh = event.target.username.value;
+   const emailh = event.target.email.value;
+   const phoneh = event.target.phone.value;
 
-
-// Implement the code as in video but with one extra 'Edit' button in 'li'
-const form = document.querySelector('form')
-const fruits = document.querySelector('.fruits')
-// const btn = document.createElement('button')
-const btnText = "Edit"
-// btn.appendChild(btnText);
-// btn.className = 'edit-btn'
-// console.log(btn)
-const addbtn = document.querySelectorAll('.fruit')
-for(let i = 0 ; i<addbtn.length ; i++){
-    const editBtn = document.createElement('button'); 
-    editBtn.textContent = btnText;
-    editBtn.className = 'edit-btn';
-    addbtn[i].appendChild(editBtn);
-    // addbtn[i].appendChild(btn)
+   localStorage.setItem('Username' , nameh);  
+   localStorage.setItem('Email' , emailh);
+   localStorage.setItem('Phone' , phoneh);
 }
-form.addEventListener('submit',function(event){
-        event.preventDefault();
-        const fruittoadd = document.getElementById('fruit-to-add')
-        const newli = document.createElement('li')
-        newli.innerHTML = fruittoadd.value + '<button class="delete-btn">x</button>'+ '<button class="edit-btn">Edit</button>'
-        // const liText = document.createTextNode(fruittoadd.value)
-        // newli.appendChild(liText)
-        // newli.className = "fruit"
-        // const btn = document.createElement('button')
-        // const btnText = document.createTextNode('x')
-        // btn.appendChild(btnText)
-        // btn.className='delete-btn'
-        // newli.appendChild(btn)
-        fruits.appendChild(newli)
-    }
-)
-fruits.addEventListener('click' , function(event){
-    if(event.target.classList.contains('delete-btn')){
-        const fruittodelete = event.target.parentElement
-        fruits.removeChild(fruittodelete)
-    }
-})
+module.exports = savetolocalStorage;
